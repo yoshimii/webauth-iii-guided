9fc7ac6daab72244740368e93f30c2a9b8cb4941
Emma Zarate <2327788+yoshimii@users.noreply.github.com>
2019-10-16 11:48:16 -0500

    generated web token

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const Users = require('../users/users-model.js');
 
@@ -25,8 +26,12 @@ router.post('/login', (req, res) => {
     .first()
     .then(user => {
       if (user && bcrypt.compareSync(password, user.password)) {
+        //produce token
+        const token = generateToken(user)
+
         res.status(200).json({
           message: `Welcome ${user.username}!`,
+          token
         });
       } else {
         res.status(401).json({ message: 'Invalid Credentials' });
@@ -36,5 +41,21 @@ router.post('/login', (req, res) => {
       res.status(500).json(error);
     });
 });
+
+function generateToken(user) {
+
+  const payload = { //key value store
+    subject: user.id,
+    username: user.username
+  }
+
+  const secret = "and in the end the love you take is equal to the love you make";//stored in the env
+
+  const options = {
+    expiresIn: '1h'
+  }
+
+  return jwt.sign(payload, secret, options)
+}
 
 module.exports = router;
